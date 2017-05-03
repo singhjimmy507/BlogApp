4d0fa2c8a5c9c240971333ae9f14f1a50aae3895
singhjimmy507 <singhjimmy507@gmail.com>
2017-05-03 09:58:23 -0400

    Add menu button Add PostActivity-Java and XML Add Resource files elements - colors and input_outline.xml

--- app/src/main/java/com/example/jimmy/lestdomay/MainActivity.java
+++ app/src/main/java/com/example/jimmy/lestdomay/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.jimmy.lestdomay;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -36,24 +38,24 @@ public class MainActivity extends AppCompatActivity {
 
         mDatabase = FirebaseDatabase.getInstance().getReference().child("Users");//points to root directory of database
         mStorageRef = FirebaseStorage.getInstance().getReference();
-        editName = (EditText) findViewById(R.id.name);
-        editEmail = (EditText) findViewById(R.id.email);
-        mFirebasebtn = (Button)findViewById(R.id.submit);
-        mUpload = (Button)findViewById(R.id.upload);
-        mFirebasebtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-            //Create child in root object. Assign some value to database
-                String name = editName.getText().toString().trim();
-                String email = editEmail.getText().toString().trim();
-
-                HashMap<String, String> credentials = new HashMap <String, String>();
-                credentials.put("Name",name);
-                credentials.put("Email",email);
-                mDatabase.push().setValue(credentials);
-
-            }
-        });
+//        editName = (EditText) findViewById(R.id.name);
+//        editEmail = (EditText) findViewById(R.id.email);
+//        mFirebasebtn = (Button)findViewById(R.id.submit);
+//        mUpload = (Button)findViewById(R.id.upload);
+//        mFirebasebtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//            //Create child in root object. Assign some value to database
+//                String name = editName.getText().toString().trim();
+//                String email = editEmail.getText().toString().trim();
+//
+//                HashMap<String, String> credentials = new HashMap <String, String>();
+//                credentials.put("Name",name);
+//                credentials.put("Email",email);
+//                mDatabase.push().setValue(credentials);
+//
+//            }
+//        });
         //Testing some code, will work on it later
 //        mUpload.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -87,5 +89,13 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu,menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.action_add){
+            startActivity(new Intent(MainActivity.this, PostActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
